3a06fd6b8a3004c8256c49a2b7786b849218d435
agnibhu-1902 <agnibhu.mandal@gmail.com>
2026-07-12 14:47:56 +0530

    add free models

--- main.py
+++ main.py
@@ -29,9 +29,10 @@ class ImageQARequest(BaseModel):
 @app.post("/answer-image")
 async def answer_image(request: ImageQARequest):
     response = client.chat.completions.create(
-        model="openai/gpt-oss-20b:free",
+        model="cohere/north-mini-code:free",
         extra_body={
             "models": [
+                "cohere/north-mini-code:free";
                 "openai/gpt-oss-20b:free",
                 "google/gemma-4-31b-it:free",
                 "google/gemma-4-26b-a4b-it:free",
